4f1932cab1bfa828be4f5649f2cc3cbee6073559
Robert Rascanu <robert.rscn@gmail.com>
2022-03-01 01:24:26 +0200

    Create Source.cpp

---
+++ Laborator1/Ex2/Source.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#define NRMAXCIFRE 10
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int Conversie(char s[])
+{
+	int nr = 0;
+	for (int i = 0; s[i+1]; ++i)
+		nr = nr*10 + (s[i] - '0');
+	//printf("%d%s", nr, " ");
+	return nr;
+}
+int main()
+{
+	FILE* f;
+	f = fopen("Input.txt", "r");
+	int sum = 0;
+	char data[NRMAXCIFRE];
+	if (f != NULL)
+	{
+		while (fgets(data, NRMAXCIFRE, f))
+		{
+			int nr = atoi(data);
+			sum += nr;
+		}
+	}
+	printf("%d", sum);
+	return 0;
+}
