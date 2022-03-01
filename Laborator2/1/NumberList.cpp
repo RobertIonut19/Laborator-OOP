51e3118d24d4c8f45d8f0ce2c0ec235b98a4f8bb
Robert Rascanu <robert.rscn@gmail.com>
2022-03-01 18:15:46 +0200

    Create NumberList.cpp

---
+++ Laborator2/1/NumberList.cpp
@@ -0,0 +1,36 @@
+#include "NumberList.h"
+#include <stdio.h>
+
+void NumberList::Init() {
+	this->count = 0;
+}
+
+bool NumberList::Add(int x) {
+	if (count >= 10)
+		return false;
+	else{
+	numbers[count] = x;
+	count++;
+	}
+}
+
+void NumberList::Sort() {
+	bool swapped = false;
+	while (swapped == false) {
+		swapped = true;
+		for(int i = 0; i < count-1; ++i)
+			if (this->numbers[i] > this->numbers[i + 1]) {
+				swapped = false;
+				int aux = this->numbers[i];
+				this->numbers[i] = this->numbers[i+1];
+				this->numbers[i+1] = aux;
+			}
+		
+	}
+}
+
+void NumberList::Print() {
+	for (int i = 0; i < this->count; ++i)
+		printf("%d%c", this->numbers[i], ' ');
+}
+
