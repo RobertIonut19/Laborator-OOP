51729695d85473e5d2b7846dd0ab1dff5da22e72
Robert Rascanu <robert.rscn@gmail.com>
2022-03-08 17:08:20 +0200

    Create main and Math Class

---
+++ Laborator3/Ex1/Math.cpp
@@ -0,0 +1,33 @@
+#include "Math.h"
+
+int Math::Add(int x, int y) {
+	return x + y;
+}
+
+int Math::Add(int x, int y, int z) {
+	return x + y + z;
+}
+
+int Math::Add(double x, double y) {
+	return x + y;
+}
+
+int Add(double x, double y, double z) {
+	return x + y + z;
+}
+
+int Math::Mul(int x, int y) {
+	return x * y;
+}
+
+int Math::Mul(int x, int y, int z) {
+	return x * y * z;
+}
+
+int Math::Mul(double x, double y) {
+	return x * y;
+}
+
+int Mul(double x, double y, double z) {
+	return x * y * z;
+}
